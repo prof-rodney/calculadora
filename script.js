4b62243b8656f47b5419c987d39de4274f3db8a0
prof-rodney <cringeparaense@gmail.com>
2026-05-26 21:19:47 -0300

    terceira aula

--- script.js
+++ script.js
@@ -30,3 +30,81 @@ function criarBotoes() {
         areaBotoes.appendChild(botao);
     });
 }
+
+function definirClasseBotao(botao, textoBotao) {
+    if (!isNaN(textoBotao) || textoBotao === ".") {
+        botao.classList.add("btn-numero");
+    } else if (
+        textoBotao === "+" ||
+        textoBotao === "-" ||
+        textoBotao === "*" ||
+        textoBotao === "/"
+    ) {
+       botao.classList.add("btn-operador"); 
+    } else if (
+        textoBotao === "C" ||
+        textoBotao === "DEL"
+    ) {
+        botao.classList.add("btn-limpar");
+    } else if (textoBotao === "=") {
+        botao.classList.add("btn-igual");
+    } else {
+        botao.classList.add("btn-funcao");
+    }
+
+    if (textoBotao === "0") {
+        botao.classList.add("btn-zero");
+    }
+}
+
+function processarBotao(valor) {
+    if (valor === "C") {
+        limparDisplay();
+        return;
+    }
+
+    if (valor === "DEL") {
+        apagarUltimoCaracter();
+        return;
+    }
+
+    if (valor === "=") {
+        calcularResultado();
+        return;
+    }
+
+    adicionarTexto(valor);
+}
+
+function adicionarTexto(valor) {
+    expressao += valor;
+    atualizarDisplay();
+}
+
+function limparDisplay() {
+    expressao = "";
+    atualizarDisplay();
+}
+
+function apagarUltimoCaracter() {
+    expressao = expressao.slice(0,-1);
+    atualizarDisplay();
+}
+
+function atualizarDisplay() {
+    display.value = expressao;
+}
+
+function calcularResultado() {
+    try {
+        const resultado = Function("return " + expressao)();
+        expressao = String(resultado);
+        atualizarDisplay()
+
+    } catch (error) {
+        display.value = "Deu ruim";
+        expressao = "";
+    }
+}
+
+iniciarCalculadora();
